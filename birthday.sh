02344e708e416e3af3173cb611e91616a177f372
Brutus11111 <poserman@gmail.com>
2021-01-29 12:07:06 +0100

    Updated with functions.

--- birthday.sh
+++ birthday.sh
@@ -6,103 +6,47 @@
 TODAY=$(date +%m-%d)
 TOMORROW=$(date --date="-1days ago" +%m-%d)
 BDFILE=~/scripts/birthdays.text
-FESFILE=~/scripts/celebrations.text
+HFILE=~/scripts/holidays.text
+ENABLE_BIRTHDAYS=1
+ENABLE_HOLIDAYS=1
 
+################
 
-# Reset
-Color_Off='\033[0m'       # Text Reset
-
-# Regular Colors
-Black='\033[0;30m'        # Black
-Red='\033[0;31m'          # Red
-Green='\033[0;32m'        # Green
-Yellow='\033[0;33m'       # Yellow
-Blue='\033[0;34m'         # Blue
-Purple='\033[0;35m'       # Purple
-Cyan='\033[0;36m'         # Cyan
-White='\033[0;37m'        # White
-
-# Bold
-BBlack='\033[1;30m'       # Black
-BRed='\033[1;31m'         # Red
-BGreen='\033[1;32m'       # Green
-BYellow='\033[1;33m'      # Yellow
-BBlue='\033[1;34m'        # Blue
-BPurple='\033[1;35m'      # Purple
-BCyan='\033[1;36m'        # Cyan
-BWhite='\033[1;37m'       # White
-
-# Underline
-UBlack='\033[4;30m'       # Black
-URed='\033[4;31m'         # Red
-UGreen='\033[4;32m'       # Green
-UYellow='\033[4;33m'      # Yellow
-UBlue='\033[4;34m'        # Blue
-UPurple='\033[4;35m'      # Purple
-UCyan='\033[4;36m'        # Cyan
-UWhite='\033[4;37m'       # White
-
-# Background
-On_Black='\033[40m'       # Black
-On_Red='\033[41m'         # Red
-On_Green='\033[42m'       # Green
-On_Yellow='\033[43m'      # Yellow
-On_Blue='\033[44m'        # Blue
-On_Purple='\033[45m'      # Purple
-On_Cyan='\033[46m'        # Cyan
-On_White='\033[47m'       # White
-
-# High Intensity
-IBlack='\033[0;90m'       # Black
-IRed='\033[0;91m'         # Red
-IGreen='\033[0;92m'       # Green
-IYellow='\033[0;93m'      # Yellow
-IBlue='\033[0;94m'        # Blue
-IPurple='\033[0;95m'      # Purple
-ICyan='\033[0;96m'        # Cyan
-IWhite='\033[0;97m'       # White
-
-# Bold High Intensity
-BIBlack='\033[1;90m'      # Black
-BIRed='\033[1;91m'        # Red
-BIGreen='\033[1;92m'      # Green
-BIYellow='\033[1;93m'     # Yellow
-BIBlue='\033[1;94m'       # Blue
-BIPurple='\033[1;95m'     # Purple
-BICyan='\033[1;96m'       # Cyan
-BIWhite='\033[1;97m'      # White
-
-# High Intensity backgrounds
-On_IBlack='\033[0;100m'   # Black
-On_IRed='\033[0;101m'     # Red
-On_IGreen='\033[0;102m'   # Green
-On_IYellow='\033[0;103m'  # Yellow
-On_IBlue='\033[0;104m'    # Blue
-On_IPurple='\033[0;105m'  # Purple
-On_ICyan='\033[0;106m'    # Cyan
-On_IWhite='\033[0;107m'   # White
-
-
-
-# SET COLOR
-COLOR=$BIYellow
-
+BIRTHDAYS () {
 if [[ $(cat $BDFILE | grep $TODAY)  ]]; then
-  echo -e "${COLOR}TODAY'S BIRTHDAYS:"
+  echo -e "TODAY'S BIRTHDAYS:"
   cat $BDFILE | grep $TODAY
 fi
 
 if [[ $(cat $BDFILE | grep $TOMORROW)  ]]; then
-  echo -e "${COLOR}TOMORROW'S BIRTHDAYS:"
+  echo -e "TOMORROW'S BIRTHDAYS:"
   cat $BDFILE | grep $TOMORROW
 fi
+}
 
-if [[ $(cat $FESFILE | grep $TODAY)  ]]; then
-  echo -e "${COLOR}TODAY'S FESTIVALS:"
-  cat $FESFILE | grep $TODAY
+################
+
+HOLIDAYS () {
+if [[ $(cat $HFILE | grep $TODAY)  ]]; then
+  echo -e "TODAY'S HOLIDAYS:"
+  cat $HFILE | grep $TODAY
 fi
 
-if [[ $(cat $FESFILE | grep $TOMORROW)  ]]; then
-  echo -e "${COLOR}TOMORROW'S FESTIVALS:"
-  cat $FESFILE | grep $TOMORROW
+if [[ $(cat $HFILE | grep $TOMORROW)  ]]; then
+  echo -e "TOMORROW'S HOLIDAYS:"
+  cat $HFILE | grep $TOMORROW
 fi
+}
+
+################
+
+if [[ "$ENABLE_BIRTHDAYS" == 1 ]]; then
+  BIRTHDAYS
+fi
+
+if [[ "$ENABLE_HOLIDAYS" == 1 ]]; then
+  HOLIDAYS
+fi
+
+
+
